e718a574d311ae023d6cbe21d3702fd2e15947bf
MLF <876952444@.com>
2021-04-17 23:42:05 +0800

    002-redis集成

--- src/main/java/com.alibaba/controller/EmpController.java
+++ src/main/java/com.alibaba/controller/EmpController.java
@@ -2,6 +2,7 @@ package com.alibaba.controller;
 
 import com.alibaba.model.Emp;
 import com.alibaba.service.IEmpService;
+import com.alibaba.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class EmpController {
     @Autowired
     private IEmpService empService;
+    @Autowired
+    private RedisUtil redisUtil;
     @RequestMapping("/select")
     public ModelAndView selectUser() throws Exception {
         ModelAndView mv = new ModelAndView();
         Emp emp = empService.selectEmp(7369);
+        redisUtil.set(emp.getEmpno().toString(),emp);
         mv.addObject("emp", emp);
         mv.setViewName("emp");
         return mv;
